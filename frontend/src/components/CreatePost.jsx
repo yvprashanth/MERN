f6685af7751c012e0d41582c4ae6b80868fbc5eb
Prashanth <yvprashanth@gmail.com>
2019-10-31 20:41:52 -0700

    More changes

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -3,43 +3,69 @@ import { Container, Row, Col, Form, Button, DropdownButton, MenuItem, Dropdown }
 import { connect } from 'react-redux'
 import { fetchCategories } from '../actions'
 import _ from 'lodash';
-import DropdownItem from 'react-bootstrap/DropdownItem';
 
 class CreatePost extends Component { 
+    constructor(props){
+        super(props)
+        this.state = { title:'My Catchy Title', content: "Random Content", author: "King of the world", category:"" }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
  
     componentWillMount() {
         this.props.myfetchCategories();
     }
 
+    handleChange = (e) => {
+        /*
+          Because we named the inputs to match their
+          corresponding values in state, it's
+          super easy to update the state
+        */
+        console.log(e.target.value)
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(event);
+        const data = new FormData(event.target);
+        fetch('/posts', {
+            method: 'POST',
+            headers: new Headers({
+                'Authorization': 'Basic '+btoa('username:password'), 
+            }), 
+            body: event,
+        });
+    }
+
     render(){
         const { myStaticCategories } = this.props;
-        console.log(typeof myStaticCategories);
         
         return(
             <div>
                 <Container>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Row>
                             <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control size="lg" type="text" placeholder="Title for the post" />
+                            <Form.Control size="lg" type="text" name="title" placeholder={this.state.title} onChange={this.handleChange}/>
                             </Form.Group>
                         </Form.Row>
 
                         <Form.Group controlId="formGridContent">
                             <Form.Label>Content</Form.Label>
-                            <Form.Control placeholder="Any content you want to enter" />
+                            <Form.Control placeholder={this.state.content} name="content" onChange={this.handleChange} />
                         </Form.Group>
 
                         <Form.Row>
                             <Form.Group as={Col} controlId="formGridAuthor">
                             <Form.Label>Author</Form.Label>
-                            <Form.Control />
+                            <Form.Control placeholder={this.state.author} name="author" onChange={this.handleChange} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridState">
                             <Form.Label>Category</Form.Label>
-                            <select className="form-control">
+                            <select className="form-control" onChange={this.handleChange}>
                                 <option value="" className="disabled">Select Category</option>
                                 {_.map(myStaticCategories, category => (
                                     <option
@@ -54,7 +80,7 @@ class CreatePost extends Component {
                         </Form.Row>
 
 
-                        <div class="col-md-4 center-block">
+                        <div className="col-md-4 center-block">
                             <Button variant="primary" type="submit" className="text-center">
                                 Submit
                             </Button>
